681c42fcc674866dc4f76b0ad463c04d14d14d84
aereeeee <dofl5576@gmail.com>
2021-01-17 23:03:28 +0900

    Add coin change second solution

--- algorythm/dp/coin_change.ts
+++ algorythm/dp/coin_change.ts
@@ -13,3 +13,24 @@ function coinChange(coins: number[], amount: number): number {
   }
   return dp[amount] === Infinity ? -1 : dp[amount];
 }
+
+//top-down (memoization)
+//time O(M) space O(M)
+function coinChange2(
+  coins: number[],
+  amount: number,
+  dp: Map<number, number> = new Map()
+): number {
+  if (dp.has(amount)) return dp.get(amount);
+  if (amount < 0) return -1;
+  if (amount === 0) return 0;
+
+  let min = Infinity;
+  for (let coin of coins) {
+    const count = coinChange2(coins, amount - coin, dp);
+    if (count >= 0) min = Math.min(min, count);
+  }
+  
+  min === Infinity ? dp.set(amount, -1) : dp.set(amount, min + 1);
+  return dp.get(amount);
+}
